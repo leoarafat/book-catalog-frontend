de712f2993d06bf55ce3d036a6522dd9d0e058e0
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-07-14 02:19:41 +0800

    create register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import Hero from "./components/ui/Hero";
 import Login from "./pages/Login";
 import Navbar from "./pages/Navbar";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
       <div>
         <Navbar />
-
         <Hero />
         <Login />
+        <Register />
       </div>
     </>
   );
---
+++ src/pages/TopBooks.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const TopBooks = () => {
+  return <div>TopBooks</div>;
+};
